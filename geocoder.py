be513207084bc7c3aaef395e80e4a17b987de71a
erikka-22 <iwasaki.pingoo@gmail.com>
2023-01-30 00:33:55 +0900

    不要なコメントを削除

--- geocoder.py
+++ geocoder.py
@@ -10,13 +10,6 @@ API_KEY = os.environ.get("API_KEY")
 df = pd.read_csv('address.csv')
 
 addresses = df['地域']
-# print(addresses[0])
-
-# url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + API_KEY
-# url = 'https://maps.googleapis.com/maps/api/geocode/json?address=東京都品川区西五反田7-22-17TOCビル12階&key=' + API_KEY
-# print(requests.get(url).json())
-
-# ret = requests.get(url).json()['results'][0]['geometry']['location']
 
 for i, address in enumerate(addresses, 0):
   url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + API_KEY
